40012e64f9692b1002e4cddadc49b530e8329e58
faintpixel <artomizer@sketchdaily.net>
2023-08-26 15:27:38 -0600

    updating auth0 callback

--- UI/skd-ref-site/src/environments/environment.prod.ts
+++ UI/skd-ref-site/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://testapi.sketchdaily.net/ReferenceSite/',
   imageUrl: 'https://files.sketchdaily.net/references',
-  auth0RedirectUri: 'http://reference.sketchdaily.net/callback/en'
+  auth0RedirectUri: 'http://reference-test.sketchdaily.net/callback/en'
 };
