693e584db93a7c22e307db0761d72b5683e37aec
faintpixel <artomizer@sketchdaily.net>
2023-09-09 10:24:30 -0600

    css mode support

--- UI/skd-ref-site/src/app/header/header.component.ts
+++ UI/skd-ref-site/src/app/header/header.component.ts
@@ -10,14 +10,40 @@ import { LanguageService } from '../language.service';
 export class HeaderComponent implements OnInit {
 
   selectedLanguage = 'en';
+  cssModeIcon = 'sun';
 
   constructor(public languageService: LanguageService) { }
 
   ngOnInit() {
     this.selectedLanguage = this.languageService.language;
+    this.loadCssMode();
   }
 
   changeLanguage() {
     this.languageService.redirectToLanguageHome(this.selectedLanguage);
+  }
+
+  loadCssMode() {
+    const lightCSS = document.getElementById('light-css') as HTMLLinkElement;
+    if (localStorage.getItem('cssMode') == 'light' && lightCSS.disabled) {
+      this.toggleCssMode();
+    }
+  }
+
+  toggleCssMode() {
+    const lightCSS = document.getElementById('light-css') as HTMLLinkElement;
+    const darkCSS = document.getElementById('dark-css') as HTMLLinkElement;
+
+    if (lightCSS.disabled) {
+      darkCSS.setAttribute('disabled', 'disabled');
+      lightCSS.removeAttribute('disabled');
+      localStorage.setItem('cssMode', 'light');
+      this.cssModeIcon = 'moon';
+    } else {
+      lightCSS.setAttribute('disabled', 'disabled');
+      darkCSS.removeAttribute('disabled');
+      localStorage.setItem('cssMode', 'dark');
+      this.cssModeIcon = 'sun';
+    }    
   }
 }
